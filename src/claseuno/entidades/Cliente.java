21647b1b6eab9db500399ea248aee10e68bd2c64
Pity879 <141643104+Pity879@users.noreply.github.com>
2023-08-07 11:18:20 -0300

    Initial commit

---
+++ src/claseuno/entidades/Cliente.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package claseuno.entidades;
+
+/**
+ *
+ * @author 54264
+ */
+public class Cliente {
+    
+}
